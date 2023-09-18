9d0d6544e7bb15b60cbf0d0f25094ffefdd5d1e0
johnferneymontoyaramirez <jfmontoya@ins.com.co>
2023-09-18 18:06:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es el ejercicio 23 correspondiente al capitulo 4
---
+++ src/main/java/john/ejercicio23cap4poogui/Ejercicio23cap4poogui.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package john.ejercicio23cap4poogui;
+
+/**
+ *
+ * @author johnferneymontoyaramirez
+ */
+public class Ejercicio23cap4poogui {
+
+    public static void main(String[] args) {
+	    Form form = new Form();
+	    form.setVisible(true);
+    }
+}
